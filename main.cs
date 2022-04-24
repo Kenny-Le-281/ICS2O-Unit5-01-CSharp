d483a0e1a9f07722e4d158185809ba71c7200845
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-04-24 01:21:06 +0000

    fixed whitespacing

--- main.cs
+++ main.cs
@@ -13,8 +13,8 @@ class Program
         int guess;
         Random randomNumber = new Random();
         int generatedNumber = randomNumber.Next(1, 6);
-      
-        // input 
+
+        // input
         Console.WriteLine("This program guesses a number between 1-6");
 
         Console.WriteLine("");
